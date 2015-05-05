2ee7e3154d385398edf4eee4721e5cf6604fcf60
Xiao Wang <xiaowang862@gmail.com>
2015-05-05 17:21:05 -0400

    add dcft_sort_mo_tpdm function

---
+++ dcft_sort_mo_tpdm.cc
@@ -0,0 +1,11 @@
+#include "integraltransform.h"
+#include <libpsio/psio.hpp>
+#include <libciomr/libciomr.h>
+#include <libqt/qt.h>
+#include <libiwl/iwl.hpp>
+#include "integraltransform_functors.h"
+#include "psifiles.h"
+#include "mospace.h"
+#define EXTERN
+#include <libdpd/dpd.gbl>
+
